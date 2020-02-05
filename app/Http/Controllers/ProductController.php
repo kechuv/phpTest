0c4825ac465d53773b1691a2cf3f1e83fc87593a
Kheops Valencia <kheopsvalencia@gmail.com>
2020-02-04 21:35:35 -0600

    init
    + Added database
    + Added controller for Vue SPA
    + Added Product model
    + Added Product controller
    + Added Product API routes
    + Added Frontend submodule

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use Illuminate\Support\Str;
+
+class ProductController extends Controller
+{
+    public function index() {
+        return Product::all();
+    }
+    public function show(Product $product) {
+        return $product;
+    }
+    public function store(Request $request) {
+        $slug = Str::slug($request->input('name'), '-');
+        $request->request->add(['slug' => $slug]);
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
+    public function update(Request $request, Product $product) {
+        $product->update($request->all());
+        return response()->json($product, 200);
+    }
+    public function delete(Product $product) {
+        $product->delete();
+        return response()->json(null, 204);
+    }
+}
